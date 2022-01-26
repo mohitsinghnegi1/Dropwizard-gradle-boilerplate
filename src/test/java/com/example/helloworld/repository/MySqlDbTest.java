56c020500f7f9e47261f5ebc0c6cd6af0b439470
mohitsinghnegi1 <mohitsin3232@gmail.com>
2022-01-26 22:46:22 +0530

    Added unit test

--- src/test/java/com/example/helloworld/repository/MySqlDbTest.java
+++ src/test/java/com/example/helloworld/repository/MySqlDbTest.java
@@ -1,12 +1,17 @@
 package com.example.helloworld.repository;
 
 import com.example.helloworld.entity.Author;
+import com.example.helloworld.entity.Books;
+import com.example.helloworld.interfaces.IAuthorInfo;
+import com.example.helloworld.interfaces.IBookInfo;
 import com.example.helloworld.models.Bindings;
 import com.example.helloworld.services.DependencyManager;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class MySqlDbTest {
@@ -21,33 +26,83 @@ public class MySqlDbTest {
 
     @Test
     public void testGetAuthorById() {
+
+        IAuthorInfo authorInfo = db.getAuthorById("authorId100");
+        assertTrue(authorInfo instanceof IAuthorInfo || authorInfo == null);
     }
 
     @Test
     public void testGetAuthors() {
+
+        List<IAuthorInfo> authors = db.getAuthors();
+
+        assertNotNull(authors);
+
+        if (authors.size() > 0) {
+            assertTrue(authors.get(0) instanceof IAuthorInfo);
+        } else {
+            assertTrue(authors.size() == 0);
+        }
+
     }
 
     @Test
     public void testGetBooksOfAuthor() {
-    }
 
-    @Test
-    public void testPrintList() {
+        List<IBookInfo> books = db.getBooksOfAuthor("authorId2");
+        assertNotNull(books);
+
+        if (books.size() > 0) {
+            assertTrue(books.get(0) instanceof IBookInfo);
+        } else {
+            assertTrue(books.size() == 0);
+        }
     }
 
     @Test
     public void testGetAllBooks() {
+
+        List<IBookInfo> books = db.getAllBooks();
+
+        assertNotNull(books);
+
+        if (books.size() > 0) {
+            assertTrue(books.get(0) instanceof IBookInfo);
+        } else {
+            assertTrue(books.size() == 0);
+        }
     }
 
     @Test
     public void testAddNewBook() {
+
+        int prevNoOfBooks = db.getAllBooks().size();
+
+        boolean isBookAdded = db.addNewBook(new Books("Book30", new Author("author30")));
+
+        int curNoOfBooks = db.getAllBooks().size();
+
+        if (isBookAdded) {
+            assertTrue(prevNoOfBooks + 1 == curNoOfBooks);
+        } else {
+            assertTrue(prevNoOfBooks == curNoOfBooks);
+        }
+
     }
 
     @Test
     public void testAddNewAuthor() {
-        assertTrue(db.addNewAuthor(new Author("AuthorId100")));
 
-        // Existing Author should not be added to database
-        assertFalse(db.addNewAuthor(new Author("AuthorId100")));
+        int prevNoOfAuthors = db.getAuthors().size();
+
+        boolean isAuthorAdded = db.addNewAuthor(new Author("AuthorId100"));
+
+        int curNoOfAuthors = db.getAuthors().size();
+
+        if (isAuthorAdded) {
+            assertTrue(prevNoOfAuthors + 1 == curNoOfAuthors);
+        } else {
+            assertTrue(prevNoOfAuthors == curNoOfAuthors);
+        }
     }
 }
